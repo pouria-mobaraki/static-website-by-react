27d8c44d443f74b0387a85aa01bc7aa29bef1716
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-03 13:02:12 +0330

    writting part of navbar js and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar.js';
+
 
 function App() {
   return (
     <div className="App">
-    <h1>welcome to my  static website</h1>
+    <Navbar/>
+    
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,21 @@
-import React,{Component} from 'react' ;
+import React from 'react' ;
+import './css/Navbar.css'
 
-class Navbar extends Component{
-    render(){
+
+const Navbar = () => {
         return(
-            <h1>This is the navbar</h1>
+           <div className='navbar'>
+            <a href="/" className='logo'>Fullyworld Web Tutorials</a>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+           </div>
         );
-    }
+    
 }
 
 export default Navbar ;
