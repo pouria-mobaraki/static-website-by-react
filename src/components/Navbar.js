67703fc9d035fb713a687dae71680161317f2df7
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-03 22:19:35 +0330

    completed navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react' ;
-import './css/Navbar.css'
+import './css/Navbar.css';
 
 
 const Navbar = () => {
@@ -7,7 +7,7 @@ const Navbar = () => {
            <div className='navbar'>
             <a href="/" className='logo'>Fullyworld Web Tutorials</a>
             <ul>
-                <li><a href="#home">Home</a></li>
+                <li className="active"><a href="#home">Home</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#portfolio">Portfolio</a></li>
