240b6e9c67a68aa9666e962c6036b840f8d9c13d
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-04 23:49:07 +0330

    add services section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import Banner from './components/Banner.js';
+import Services from './components/Services.js';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
     <Navbar/>
     <Banner/>
-    
+    <Services/>
     </div>
   );
 }
