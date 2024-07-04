23b62f9bba9680fbfab3daf3e83f3746b7d0b82a
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-07-04 18:53:48 +0330

    add banner.js and banner.css and completed them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
+import Banner from './components/Banner.js';
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Banner/>
     
     </div>
   );
---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './css/Banner.css'
+
+const Banner = () =>{
+    return(
+        <div className="banner">
+            <div className="banner-info">
+                <h1>WELCOME TO <br/> FULLYWORD TUTORIALS</h1>
+                {/* <p>Make anything possible</p> */}
+                <a href="#services">Explore</a>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Banner ;
